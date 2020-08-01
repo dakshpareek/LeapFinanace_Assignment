b8e66b30ef637afdaaaf5eca23ca358498b56193
Daksh Pareek <dakshpareek7@gmail.com>
2020-08-02 00:26:08 +0530

    Get method added

--- src/main/java/com/assignment/project/controller/ScraperController.java
+++ src/main/java/com/assignment/project/controller/ScraperController.java
@@ -4,9 +4,7 @@ import com.assignment.project.dto.UrlDTO;
 import com.assignment.project.service.ScraperService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.io.IOException;
@@ -18,6 +16,12 @@ public class ScraperController {
     private ScraperService scraperService;
     public ScraperController(ScraperService scraperService) {
         this.scraperService = scraperService;
+    }
+
+    @GetMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
     }
 
     @PostMapping("/content")
